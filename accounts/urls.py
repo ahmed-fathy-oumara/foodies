2b9a6cefb1cc5da0ed2819e1f31b8196a08f5767
ahmed_fathy_oumara <ahmedfathyoumara@gmail.com>
2023-09-13 16:40:53 +0300

    vendor model and vendor registration form

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register-user/', views.registerUser, name='register_user'),
+    path('register-vendor/', views.registerVendor, name='register_vendor'),
 ]
